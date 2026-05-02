56a36d23c415c5ebeeb84948eeb69377bb92277c
cedric francoys <cedricfrancoys@gmail.com>
2026-05-02 18:04:26 +0200

    updated

--- build.sh
+++ build.sh
@@ -36,7 +36,6 @@ while [ "$i" -lt "$COUNT" ]; do
 
   SRC_PATH="$CLONE_DIR/$PATH_SRC"
 
-  # Gestion target = "."
   if [ "$TARGET" = "." ]; then
     DEST_PATH="$DOCS"
     NAV_PREFIX=""
@@ -47,35 +46,38 @@ while [ "$i" -lt "$COUNT" ]; do
 
   mkdir -p "$DEST_PATH"
 
-  # Copie robuste (support dossier vide)
+  ERROR=0
+
   if [ -d "$SRC_PATH" ]; then
     cp -r "$SRC_PATH/." "$DEST_PATH/"
   else
-    echo "️Error - Missing path: $SRC_PATH"
-	i=$((i + 1))
-    continue
+    echo "Error - Missing path: $SRC_PATH"
+    ERROR=1
   fi
 
-  # NAV
-  if [ -f "$DEST_PATH/nav.yml" ]; then
+  if [ "$ERROR" -eq 0 ]; then
+
+    # Ligne vide uniquement entre blocs (pas avant le premier)
+    [ "$i" -gt 0 ] && echo "" >> "$NAV_FILE"
+
     echo "  - $NAME:" >> "$NAV_FILE"
 
-    # Si root → pas de préfixe
-    if [ "$TARGET" = "." ]; then
-      sed 's/^/    /' "$DEST_PATH/nav.yml" >> "$NAV_FILE"
+    if [ -f "$DEST_PATH/nav.yml" ]; then
+
+      if [ "$TARGET" = "." ]; then
+        sed 's/^/    /' "$DEST_PATH/nav.yml" >> "$NAV_FILE"
+      else
+        sed "s|: '|: '$NAV_PREFIX|g" "$DEST_PATH/nav.yml" | sed 's/^/    /' >> "$NAV_FILE"
+      fi
+
     else
-      # préfixer les chemins
-      sed "s|: |: $NAV_PREFIX|g" "$DEST_PATH/nav.yml" | sed 's/^/    /' >> "$NAV_FILE"
+      if [ "$TARGET" = "." ]; then
+        echo "    - Home: index.md" >> "$NAV_FILE"
+      else
+        echo "    - Home: $TARGET/index.md" >> "$NAV_FILE"
+      fi
     fi
 
-  else
-    echo "  - $NAME:" >> "$NAV_FILE"
-
-    if [ "$TARGET" = "." ]; then
-      echo "    - Home: index.md" >> "$NAV_FILE"
-    else
-      echo "    - Home: $TARGET/index.md" >> "$NAV_FILE"
-    fi
   fi
 
   i=$((i + 1))
@@ -92,7 +94,9 @@ awk '
 ' mkdocs.yml > "$TMP/mkdocs.clean.yml"
 
 cat "$TMP/mkdocs.clean.yml" > mkdocs.yml
-echo "" >> mkdocs.yml
+
+# Une seule ligne vide avant nav (contrôlée)
+printf "\n" >> mkdocs.yml
 cat "$NAV_FILE" >> mkdocs.yml
 
 echo "== Done =="
